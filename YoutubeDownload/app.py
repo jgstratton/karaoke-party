95a7c2cd8240404dec70c554bba42a3ee08c5fa7
Jesse Stratton <jgs.stratton@gmail.com>
2023-11-12 21:24:18 -0500

    adjust yt-dlp settings

--- YoutubeDownload/app.py
+++ YoutubeDownload/app.py
@@ -64,7 +64,7 @@ def download_video(video_url):
 	temp_path = download_path + "/" + temp_folder
 	os.mkdir(temp_path)
 	dl_path = temp_path + "/%(title)s---%(id)s.%(ext)s"
-	file_quality =  "bestvideo[height<=720]+bestaudio/best/best"
+	file_quality = "bestvideo[ext=mp4][height<=720]+bestaudio[ext=m4a]/best[ext=mp4]/best"
 	app.logger.info(file_quality)
 	cmd = [youtubedl_path, "-f", file_quality, "-o", dl_path, "--max-downloads", "1",video_url]
 	rc = subprocess.call(cmd)
